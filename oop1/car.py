0be900f9050fc2ec737fb1734df320234bb56f4a
İrem Özdemir <irem.ozdemir@etu.edu.tr>
2024-08-17 17:05:31 +0300

    car project

--- oop1/car.py
+++ oop1/car.py
@@ -1,9 +1,27 @@
 class Car:
+
+    wheels = 4
+    num_of_cars = 0
+    #class variable
+    #tüm objeler aynı variable'ı kullanır
+
     def __init__(self,model,year,color):
         self.model = model
         self.year = year
         self.color = color
+        #her objenin kendi variable'ı vardır
+
+        Car.num_of_cars += 1
+        #her obje oluşturulduğunda 1 arttırılır
     #constructor
 
-    def drive():
-        print("you drive the car")
+    def drive(self):
+        print(f"you drive the {self.model}")
+    #self yazılmazsa hata veriyor
+
+    def stop(self):
+        print(f"you stop the {self.model}")
+    #self yazılmazsa hata veriyor
+
+    def describe(self):
+        print(f"model:{self.model} year:{self.year} color:{self.color}")
--- oop1/main.py
+++ oop1/main.py
@@ -1,4 +1,4 @@
-from oop1.car import Car
+from car import Car
 
 car1 = Car("mustang",2022,"blue")
 car2 = Car("corvette",2023,"red")
@@ -7,10 +7,22 @@ car3 = Car("charger",2024,"green")
 
 print(car1)
 #adresi yazar
-
 print(car1.__dict__)
 #dictionary halinde yazar
-
 print(car1.model)
 print(car1.year)
 print(car1.color)
+
+car1.drive()
+car1.stop()
+car1.describe()
+
+print(car1.wheels)
+print(car2.wheels)
+print(car3.wheels)
+print(Car.wheels)
+
+print(car1.num_of_cars)
+print(car2.num_of_cars)
+print(car3.num_of_cars)
+print(Car.num_of_cars)
