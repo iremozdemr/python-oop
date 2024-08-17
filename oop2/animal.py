9a6f991d60f97cc3bd4e6089700f2bafb3b9ff9a
İrem Özdemir <irem.ozdemir@etu.edu.tr>
2024-08-17 17:14:52 +0300

    added animal

---
+++ oop2/animal.py
@@ -0,0 +1,32 @@
+class Animal:
+    def __init__(self,name):
+        self.name = name
+        self.is_alive = True
+
+    def eat(self):
+        print(f"{self.name} is eating")
+
+    def sleep(self):
+        print(f"{self.name} is sleeping")
+
+class Dog(Animal):
+    def speak(self):
+        print("woof!")
+
+class Cat(Animal):
+    def speak(self):
+        print("meow!")
+
+class Mouse(Animal):
+    def speak(self):
+        print("squeek!")
+
+dog = Dog("scooby")
+cat = Cat("garfield")
+mouse = Mouse("mickey")
+
+print(dog.name)
+print(dog.is_alive)
+dog.eat()
+dog.sleep()
+dog.speak()
